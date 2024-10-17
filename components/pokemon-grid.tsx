236be7b00f2d3db33cc1e7696dcd6b43bd07784a
samy0307 <samet.sevimli07@gmail.com>
2024-10-17 10:05:17 +0200

    update

--- components/pokemon-grid.tsx
+++ components/pokemon-grid.tsx
@@ -21,29 +21,32 @@ export function PokemonGrid({ pokemonList }: PokememonCardProps) {
 
   return (
     <>
-      <div>
-        <h3 className="text-2xl underline py-6 text-center">
-          Search For Your Pokemon!
-        </h3>
-        <div className="grid w-full max-w-sm items-center gap-1.5">
-          <Label htmlFor="pokemonName">Pokemon Name</Label>
-          <Input
-            type="text"
-            value={searchText}
-            autoComplete="off"
-            id="pokemonName"
-            placeholder="Charizard, Pikachu, etc"
-            onChange={(e) => setSearchText(e.target.value)}
-          />
+      <div className="flex flex-col items-center justify-center min-h-screen">
+        <div>
+          <h3 className="text-2xl underline py-6 text-center">
+            Search For Your Pokemon!
+          </h3>
+          <div className="grid w-full max-w-sm items-center gap-1.5">
+            <Label htmlFor="pokemonName">Pokemon Name</Label>
+            <Input
+              type="text"
+              value={searchText}
+              autoComplete="off"
+              id="pokemonName"
+              placeholder="Charizard, Pikachu, etc"
+              onChange={(e) => setSearchText(e.target.value)}
+            />
+          </div>
+          <h3 className="text-3xl pt-12 pb-6 text-center text-yellow-400">
+            Pokemon Collection
+          </h3>
         </div>
-        <h3 className="text-3xl pt-12 pb-6 text-center text-yellow-400">
-          Pokemon Collection
-        </h3>
-      </div>
-      <div className="mb-32 grid grid-cols-3 text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-3 lg:text-left">
-        {filteredPokemonList.map((pokemon: any) => {
-          return <PokemonCard key={pokemon.id} name={pokemon.name} />;
-        })}
+
+        <div className="mb-32 grid grid-cols-2 lg:grid-cols-3 text-center w-full max-w-5xl lg:text-left">
+          {filteredPokemonList.map((pokemon: any) => {
+            return <PokemonCard key={pokemon.id} name={pokemon.name} />;
+          })}
+        </div>
       </div>
     </>
   );
